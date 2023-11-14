7d131ed38bbde90fb38fc865f884f5f0c592e403
Yilei Wang <y362wang@uwaterloo.ca>
2023-11-14 01:43:07 -0500

    moved favicon.ico to build directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   
   theme: {
     extend: {
+      colors:{
+        papayawhip: {
+          light: '#fef4e4',
+          DEFAULT: '#ffefd5',
+          dark: '#fee5bc',
+        }
+      },
       screens: { 
         'widescreen': {'raw':'(min-aspect-ratio:3/2)'},
         'tallscreen': {'raw':'(max-aspect-ratio:13/20)'},
@@ -15,7 +22,7 @@ module.exports = {
           '80%': {transform:'scaleY(1.2)' }, /*give the menu a little bounce*/
           '100%': {transform:'scaleY(1)' },
         },
-      },/*add key frmaes in an animation*/
+      }, /*add key frmaes in an animation*/
       animation: {
         'open-menu':'open-menu 0.5s ease-in-out forwards',
       },/*specify the animation and name it*/
